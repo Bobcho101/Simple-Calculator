f265cf4f3b89baedf46585b058178df07bc0be5a
Bobcho101 <bgbobi878@gmail.com>
2025-02-20 00:58:23 +0200

    added function that adds the nums and C btn clears the input

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
-export default function Button(props){
+export default function Button({value, onClick}){
     // const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     // const operators = ['+', '-', '*', '/', '%', '='];
     // const uniqueOperators = ['C'];
 
     return(
     <>
-        <button className="calc-button">{props.value}</button>
+        <button onClick={() => onClick(value)} className="calc-button">{value}</button>
     </>
     )
 }
