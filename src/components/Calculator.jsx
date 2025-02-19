dbfe0934aa00b3f2ee8c9dc3ac798e59873c31ea
Bobcho101 <bgbobi878@gmail.com>
2025-02-20 00:58:38 +0200

    that too

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import Button from "./Button";
 
 export default function Calculator(){
+    const [inputVal, setInputVal] = useState('');
+    function handleButtonClick(value){
+        if(value === 'C'){
+            setInputVal(inputVal => inputVal = '');
+        } else{
+            setInputVal(inputVal => inputVal += value)
+        }
+        
+    }
     return(
         <>
             <div id="calculator-container">
                 <div id="input-output-container">
-                    <input type="text" id="input-output-field" readOnly></input>
+                    <input type="text" value={inputVal} id="input-output-field" readOnly></input>
                 </div>
                 <div id="buttons-container">
                     <div id="operators-container">
-                        <Button value={'C'}/>
+                        <Button onClick={handleButtonClick} value={'C'}/>
                         <Button value={'='}/>
                         <Button value={'+'}/>
                         <Button value={'-'}/>
@@ -18,16 +28,16 @@ export default function Calculator(){
                         <Button value={'%'}/>
                     </div>
                     <div id="numbers-container">
-                        <Button value={1}/>
-                        <Button value={2}/>
-                        <Button value={3}/>
-                        <Button value={4}/>
-                        <Button value={5}/>
-                        <Button value={6}/>
-                        <Button value={7}/>
-                        <Button value={8}/>
-                        <Button value={9}/>
-                        <Button value={0}/>
+                        <Button onClick={handleButtonClick} value={1}/>
+                        <Button onClick={handleButtonClick} value={2}/>
+                        <Button onClick={handleButtonClick} value={3}/>
+                        <Button onClick={handleButtonClick} value={4}/>
+                        <Button onClick={handleButtonClick} value={5}/>
+                        <Button onClick={handleButtonClick} value={6}/>
+                        <Button onClick={handleButtonClick} value={7}/>
+                        <Button onClick={handleButtonClick} value={8}/>
+                        <Button onClick={handleButtonClick} value={9}/>
+                        <Button onClick={handleButtonClick} value={0}/>
                     </div>
                 </div>
             </div>
